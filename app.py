3c9bdd39285383b811aa5a37c5605fee3a85744f
saman <itsawmi@gmail.com>
2023-08-19 03:26:38 +0330

    added

---
+++ app.py
@@ -0,0 +1,19 @@
+def index() :
+
+    weight = int(input("please send me your weight(kg) forexample=72 :"))
+    height = float(input("please send me your height(m) forexample=1.77 :"))
+
+    bmi = weight / (height * height)   
+    print(bmi)
+    if bmi<18 : 
+     print (f"Severe underweight")
+
+    elif 18 < bmi < 25:
+     print (f"normal")
+
+    elif bmi>25 :        
+     print (f"Overweight")
+
+    else :
+     pass
+index()
